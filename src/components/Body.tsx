9af7582b34b963a8b6f84e782b3d4629c40eb0a7
Alesh Houdek <mail@alesh.com>
2023-11-01 23:26:41 -0600

    update content, add line numbers, tweak styling

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -2,25 +2,47 @@ import React from "react";
 import styled from "styled-components";
 import { ghostCursor } from "../utils/ghostCursor";
 
+const numbers = Array(1000)
+  .fill("")
+  .map((el, i) => <div>{i}</div>);
+
+const Numbers = styled((props: any) => {
+  return <p className={props.className}>{numbers}</p>;
+})`
+  position: absolute;
+  left: -40px;
+  height: 100%;
+  overflow: hidden;
+  text-align: right;
+  color: rgba(0, 0, 0, 0.5);
+  @media (max-width: 780px) {
+    display: none;
+  }
+`;
+
 const Body = styled((props: any) => {
   React.useEffect(function cursor() {
     ghostCursor();
   }, []);
 
-  return <div className={props.className}>{props.children}</div>;
+  return (
+    <main className={props.className}>
+      <Numbers />
+      <div>{props.children}</div>
+    </main>
+  );
 })`
+  position: relative;
   box-sizing: border-box;
   max-width: 700px;
   margin-left: auto;
   margin-right: auto;
-  padding: 18px 3px;
-  border-right: 1px solid #cccccc;
+  padding: 0 10px 18px 10px;
   border-left: 1px solid #cccccc;
   min-height: 100vh;
   color: #333333;
   & h1 {
     color: var(--accent-color);
-    margin-bottom: 18px;
   }
   & ul {
     padding-left: 20px;
@@ -31,7 +53,7 @@ const Body = styled((props: any) => {
   & a:visited {
     color: --accent-color;
   }
-  @media (max-width: 650px) {
+  @media (max-width: 710px) {
     border: none;
     padding-left: 20px;
     padding-right: 20px;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -53,82 +53,96 @@ const VisuallyHidden = styled.span`
   }
 `;
 
+const Emoji = styled.span`
+  font-size: 24px;
+  line-height: 0;
+  position: relative;
+  top: 2px;
+  margin-right: 0.25em;
+`;
+
 const IndexPage: React.FC = () => (
-  <>
-    <Body>
-      <h1>Alesh Houdek</h1>
-      <h2>I'm a web developer. Mostly React. Not too much.</h2>
-      <p>
-        Just tossing this together because my old site requires an old version
-        of PHP, and my web host is bugging me about it (actually, I think
-        they're now charging me $5 a month or something extra to support it??)
-        and it's not worth upgrading.
-      </p>
-      <h2>A few recent projects</h2>
-      <ul>
-        <li>
-          <a
-            href="https://marketplace.visualstudio.com/items?itemName=alesh-houdek.steno-light-theme"
-            target="_blank"
-          >
-            Steno theme for VSCode
-          </a>
-          . Dark mode is{" "}
-          <a
-            href="https://tidbits.com/2019/05/31/the-dark-side-of-dark-mode/"
-            target="_blank"
-          >
-            bad
-          </a>
-          . Green{" "}
-          <a href="https://www.shiftelearning.com/blog/how-do-colors-influence-learning">
-            boosts
-          </a>{" "}
-          concentration. Maybe. Anyway, why do you think accountants' steno pads
-          are green?
-        </li>
-        <li>
-          <a href="https://randofont.alesh.com" target="_blank">
-            RandoFont
-          </a>
-          . Find a font for your next project.
-        </li>
-        <li>
-          <a href="https://albums.alesh.com/" target="_blank">
-            Album Spinner
-          </a>
-          . Spotify shuffles by song. This shuffles by album. Unfortunately,
-          it's a hard-coded list of albums. At least they're not terrible?
-        </li>
-      </ul>
-      <p>
-        Haha, I'm leaving this deliberately unfinished,{" "}
-        <a href="mailto:mail@alesh.com?subject=Mail from alesh.com">email me</a>{" "}
-        if it's been a long time and you think there's something I should add
-        here. You can also see what this site looked like circa{" "}
-        <a href="https://alesh.com/old/">2016</a> and{" "}
-        <a href="http://2021.alesh.com">2021</a>.
-      </p>
-      <section>
-        <IconLink href="https://github.com/aleshh">
-          <GitHub {...iconProps} />
-          <VisuallyHidden>GitHub</VisuallyHidden>
-        </IconLink>
-        <IconLink href="https://www.linkedin.com/in/alesh/">
-          <Linkedin {...iconProps} />
-          <VisuallyHidden>LinkedIn</VisuallyHidden>
-        </IconLink>
-        <IconLink href="https://www.instagram.com/alesh/">
-          <Instagram {...iconProps} />
-          <VisuallyHidden>Instagram</VisuallyHidden>
-        </IconLink>
-        <IconLink href="mailto:mail@alesh.com?subject=Mail sent from alesh.com">
-          <Mail {...iconProps} />
-          <VisuallyHidden>Email</VisuallyHidden>
-        </IconLink>
-      </section>
-    </Body>
-  </>
+  <Body>
+    <h1>Alesh Houdek</h1>
+    <h2>I'm a web developer. Mostly React. Not too much.</h2>
+    <p>
+      I care about <Emoji>🚀</Emoji>performance, <Emoji>🧑‍🦽</Emoji>
+      accessibility, and <Emoji>🧰</Emoji>clean code. Love working with UX
+      designers but roll my own when necessary. Currently working at Amazon.
+      Svelte-curious. This quick n dirty website is made with Gatsby.
+    </p>
+    <h2>A few recent projects</h2>
+    <ul>
+      <li>
+        <a href="https://forker.alesh.com/" target="_blank">
+          Forker
+        </a>{" "}
+        → A front-end for Pitchfork's new album reviews that shows the rating on
+        the list page. This was a chance to play around with Next, and a
+        url-first approach to state management, and Pitchfork's undocumented
+        API. (Currently working on filtering!)
+      </li>
+      <li>
+        <a href="https://albums.alesh.com/" target="_blank">
+          Album Spinner
+        </a>{" "}
+        → Shows a random album and allows shuffling and opening in Spotify. The
+        interesting bits are the adaptive color palette and the Spotify API
+        integration.
+      </li>
+      <li>
+        <a href="https://randofont.alesh.com" target="_blank">
+          RandoFont
+        </a>{" "}
+        → Find a font for your next project. Uses the Google Fonts API.
+      </li>
+      <li>
+        <a
+          href="https://marketplace.visualstudio.com/items?itemName=alesh-houdek.steno-light-theme"
+          target="_blank"
+        >
+          Steno theme for VSCode
+        </a>{" "}
+        → Dark mode is{" "}
+        <a
+          href="https://tidbits.com/2019/05/31/the-dark-side-of-dark-mode/"
+          target="_blank"
+        >
+          bad
+        </a>
+        . Green{" "}
+        <a href="https://www.shiftelearning.com/blog/how-do-colors-influence-learning">
+          boosts
+        </a>{" "}
+        concentration. Maybe. Anyway, why do you think accountants' steno pads
+        are green?
+      </li>
+    </ul>
+    <p>
+      <a href="mailto:mail@alesh.com?subject=Mail from alesh.com">Email me</a>{" "}
+      if you want to chat. You can also see what this site looked like circa{" "}
+      <a href="https://alesh.com/old/">2016</a> and{" "}
+      <a href="http://2021.alesh.com">2021</a>.
+    </p>
+    <section>
+      <IconLink href="https://github.com/aleshh">
+        <GitHub {...iconProps} />
+        <VisuallyHidden>GitHub</VisuallyHidden>
+      </IconLink>
+      <IconLink href="https://www.linkedin.com/in/alesh/">
+        <Linkedin {...iconProps} />
+        <VisuallyHidden>LinkedIn</VisuallyHidden>
+      </IconLink>
+      <IconLink href="https://www.instagram.com/alesh/">
+        <Instagram {...iconProps} />
+        <VisuallyHidden>Instagram</VisuallyHidden>
+      </IconLink>
+      <IconLink href="mailto:mail@alesh.com?subject=Mail sent from alesh.com">
+        <Mail {...iconProps} />
+        <VisuallyHidden>Email</VisuallyHidden>
+      </IconLink>
+    </section>
+  </Body>
 );
 
 export default IndexPage;
